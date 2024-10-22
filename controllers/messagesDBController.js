eaa1be8db06dd5eb29c996d12858dd041e3470cd
JMathiak <jmathiak32@yahoo.com>
2024-10-22 10:20:51 -0500

    wrote functions for getting all messages, posting  a new message, getting details of specific message, and deleting specific message

--- controllers/messagesDBController.js
+++ controllers/messagesDBController.js
@@ -1,2 +1,35 @@
 const db = require('../db/queries')
 
+async function getMessages(req, res){
+    const messages = await db.getMessages()
+    console.log(messages)
+    res.render("index", {
+        title: 'Messages',
+        messages: messages
+    })
+}
+
+
+async function postNewMessage(req, res){
+    let author = req.body.author
+    let message = req.body.newMessage
+    await db.insertMessaage(author, message)
+    res.redirect("/")
+}
+
+async function getDetails(req, res){
+    let messageId = req.params.id
+    let messageObj = await db.findMessage(messageId)
+    res.render("detail", {message: messageObj})
+}
+
+async function deleteMessage(req, res){
+    let messageId = req.params.id
+    await db.deleteMessage(messageId)
+    res.redirect("/")
+}
+module.exports = {
+    getMessages,
+    postNewMessage,
+    getDetails, deleteMessage
+}
