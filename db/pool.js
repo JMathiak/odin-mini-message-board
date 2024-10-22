f29c556586490f9cdc6719f2ceb7081c8dcdd147
JMathiak <jmathiak32@yahoo.com>
2024-10-22 10:20:58 -0500

    adjusted pool for SSL

--- db/pool.js
+++ db/pool.js
@@ -4,7 +4,9 @@ require('dotenv').config()
 module.exports = new Pool({
     host: process.env.HOST,
     user: process.env.USER,
+    ssl: require,
     database: process.env.DBNAME,
     password: process.env.PASSWORD,
-    port: process.env.DBPORT
+    port: process.env.DBPORT,
+    
 });
